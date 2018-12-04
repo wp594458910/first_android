d0246c362e4a52db4b3c4beb032a741d4ce9691d
peng <594458910@qq.com>
2018-12-04 17:49:34 +0800

    fix

--- app/src/main/java/com/example/zws/myapplication/adapter/FruitAdapter1.java
+++ app/src/main/java/com/example/zws/myapplication/adapter/FruitAdapter1.java
@@ -7,11 +7,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.example.zws.myapplication.R;
 import com.example.zws.myapplication.bean.Fruit;
-
-import org.w3c.dom.Text;
 
 import java.util.List;
 
