0ed9cf556cd30a972476d6a9b76d65451db4fe1e
peng <594458910@qq.com>
2018-12-07 14:47:40 +0800

    NavigationView 练习

--- app/src/main/java/com/example/zws/myapplication/MaterialActivity.java
+++ app/src/main/java/com/example/zws/myapplication/MaterialActivity.java
@@ -1,5 +1,10 @@
 package com.example.zws.myapplication;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -10,12 +15,30 @@ import com.example.zws.myapplication.utils.ToastUtils;
 
 public class MaterialActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_material);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layput);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(getResources().getDrawable(R.drawable.ic_menu1));
+        }
+        navigationView.setCheckedItem(R.id.nav_call);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
     }
 
     @Override
@@ -35,6 +58,9 @@ public class MaterialActivity extends AppCompatActivity {
                 break;
             case R.id.settings:
                 ToastUtils.toastMessage(this, "you click settings");
+                break;
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
                 break;
             default:
         }
